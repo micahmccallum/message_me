701424d637e35173e49896ed304aea1a7f43eaa6
micahmccallum <micahmccallum@outlook.com>
2020-01-06 18:47:44 -0500

    Add message model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-
+  has_many :messages
   validates :username, presence: true,
             uniqueness: { case_sensitive: false },
-            length: { minimum: 3, maximum: 5 }
+            length: { minimum: 3, maximum: 15 }
   has_secure_password
 end

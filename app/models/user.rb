e14d65319e3a24df3ace3d19ccbad6f5bde0303d
micahmccallum <micahmccallum@outlook.com>
2020-01-06 18:24:39 -0500

    Add user model and migration

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+
+  validates :username, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 5 }
+  has_secure_password
+end

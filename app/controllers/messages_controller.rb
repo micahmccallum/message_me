c7ecfb31358435a62c21b24708fb47fc106280a6
micahmccallum <micahmccallum@outlook.com>
2020-01-06 20:15:11 -0500

    Add ability to display messages on chatroom card

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,30 @@
 class MessagesController < ApplicationController
 
+  def new
+    
+  end
+
+  def create
+    @message = Message.new(params[:body, :user_id])
+    if @message.save
+      flash[:success] = "Object successfully created"
+      redirect_to @message
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+  def index
+    @messages = Message.all
+  end
+  
+
+  def show
+    @messages = Message.all
+  end
+  
+  
+  
+
 end

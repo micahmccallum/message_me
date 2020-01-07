2e694bcf19231a754fc95d105add0dc30650ec13
micahmccallum <micahmccallum@outlook.com>
2020-01-07 15:34:56 -0500

    Add flash messages to UI

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,24 @@ class SessionsController < ApplicationController
     
   end
 
-  def create
+  def create   
     user = User.find_by(username: params[:session][:username].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:success] = "You have successfully logged in."
       redirect_to root_path
     else
-      flash.now[:danger] = "Something went wrong"
+      flash.now[:error] = "Something went wrong"
       render 'new'
     end
   end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have logged out"
+    redirect_to root_path
+  end
+    
   
   
   

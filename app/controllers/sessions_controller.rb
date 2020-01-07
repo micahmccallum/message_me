52b0d4a62030ec084c0c31ed28fb3990ed496d2a
micahmccallum <micahmccallum@outlook.com>
2020-01-06 21:26:28 -0500

    Add login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,21 @@
 class SessionsController < ApplicationController
+  
   def new
     
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in."
+      redirect_to root_path
+    else
+      flash.now[:danger] = "Something went wrong"
+      render 'new'
+    end
+  end
+  
+  
   
 end
